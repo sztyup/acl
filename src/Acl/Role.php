059a5373a547a45ab7ae55bbbfb3a4c8ebfe28a2
Sztyup <sztyup@gmail.com>
2017-12-23 20:14:04 +0100

    Add ID to role

--- src/Acl/Role.php
+++ src/Acl/Role.php
@@ -4,14 +4,21 @@ namespace Sztyup\Acl;
 
 class Role
 {
+    protected $id;
     protected $name;
     protected $title;
     protected $truth;
 
-    public function __construct($name, $title)
+    public function __construct(int $id, string $name, $title)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->title = $title;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     public function getTitle(): string
