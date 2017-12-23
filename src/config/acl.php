64794536ca14f224e9bf4225d8487b5ba4a440a1
Sztyup <sztyup@gmail.com>
2017-12-23 19:30:48 +0100

    Added new config option

--- src/config/acl.php
+++ src/config/acl.php
@@ -9,9 +9,20 @@ return [
     | Here you should list all the permission used by your application
     |
     */
-    'permissions_class' => \Sztyup\Acl\Permissions::class,
+    'permissions_class' => \Sztyup\Acl\PermissionsManager::class,
 
     'permission_to_role_class' => \Sztyup\Acl\Contracts\PermissionToRole::class,
 
     'role_to_user_class' => \Sztyup\Acl\Contracts\RoleToUser::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Inheritance
+    |--------------------------------------------------------------------------
+    |
+    | You can enable/disable permission inheritance
+    |
+    */
+
+    'inheritance' => true,
 ];
