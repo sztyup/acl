aed08182d2f442ab9ebf96cc26ad562bc1d49721
Sztyup <sztyup@gmail.com>
2017-12-23 19:46:40 +0100

    Comments

--- src/Acl/Contracts/RoleToUser.php
+++ src/Acl/Contracts/RoleToUser.php
@@ -3,8 +3,13 @@
 namespace Sztyup\Acl\Contracts;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use Sztyup\Acl\Role;
 
 interface RoleToUser
 {
+    /**
+     * @param Authenticatable $user
+     * @return Role[]
+     */
     public function getRolesForUser(Authenticatable $user): array;
 }
