f368c10e11867c3cf08b39bf0b98141cda76978d
Sztyup <sztyup@gmail.com>
2017-12-24 01:45:20 +0100

    Tree helper functionality is moved into extended Node class

---
+++ src/Acl/Node.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Sztyup\Acl;
+
+use Illuminate\Support\Collection;
+use Sztyup\Acl\Contracts\NodeRepository;
+use Tree\Builder\NodeBuilderInterface;
+use Tree\Node\NodeInterface;
+use Tree\Node\NodeTrait;
+use Tree\Visitor\YieldVisitor;
+
+abstract class Node implements NodeInterface
+{
+    use NodeTrait;
+
+    protected $truth;
+    protected $name;
+
+    public function __construct($name, $truth)
+    {
+        $this->name = $name;
+        $this->truth = $truth;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * It tells wheter the current node is given to the user regardless of the persistent state
+     *
+     * @param UsesAcl $user
+     * @return bool
+     */
+    public function apply(UsesAcl $user): bool
+    {
+        if (!is_callable($this->truth)) {
+            return false;
+        }
+
+        return call_user_func($this->truth, $user);
+    }
+
+    /**
+     * Converts an array represantation of permissions into a NodeTree
+     *
+     * @param NodeBuilderInterface $tree
+     * @param $permissions array A recursive array with the permissions
+     * @param NodeRepository $repository
+     * @return NodeBuilderInterface The NodeInterface for the root of the tree
+     */
+    public static function buildTree($tree, array $permissions, NodeRepository $repository)
+    {
+        if ($tree == null) {
+            $tree = new NodeBuilder();
+            $top = true;
+        } else {
+            $top = false;
+        }
+
+        foreach ($permissions as $child => $properties) {
+            self::buildTree(
+                $tree->tree(
+                    $repository->parse($child, $properties)
+                ),
+                $repository->getChildren($child, $properties),
+                $repository
+            );
+        }
+
+        if ($top) {
+            return $tree;
+        } else {
+            return $tree->end();
+        }
+    }
+
+    protected function filter(Node $root, callable $function, $inherits)
+    {
+        $result = new Collection();
+
+        foreach ($root->getChildren() as $child) {
+            if ($function($child->getValue())) {
+                $result[] = $inherits ? $child->getAncestorsAndSelf() : $child;
+            }
+            $result = $result->merge(
+                $this->filter($child, $function, $inherits)
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Return all permission node from the tree, for which the given callback returns true
+     *
+     * @param callable $filterFunction
+     * @param bool $inherits
+     * @return array
+     */
+    public function filterTree(callable $filterFunction, $inherits = true)
+    {
+        $result = $this->filter($this, $filterFunction, $inherits);
+
+        // Extract values from nodes
+        return array_map(
+            function (Node $node) {
+                return $node->getValue();
+            },
+            $result
+        );
+    }
+
+    public function mapWithKeys(callable $function)
+    {
+        $collection = new Collection(
+            $this->accept(new YieldVisitor())
+        );
+
+        return $collection->mapWithKeys($function);
+    }
+}
