3cba2bdbbef94f5aed4910b7bdb1e7dae31db469
Sztyup <sztyup@gmail.com>
2017-12-26 06:33:57 +0100

    Model implements Contract

--- src/Acl/Models/Role.php
+++ src/Acl/Models/Role.php
@@ -3,11 +3,17 @@
 namespace Sztyup\Acl\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Sztyup\Acl\Contracts\Role as RoleContract;
 
-class Role extends Model
+class Role extends Model implements RoleContract
 {
     public function user()
     {
         return $this->belongsToMany(config('acl.user_model'));
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
     }
 }
