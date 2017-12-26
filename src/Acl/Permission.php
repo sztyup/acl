45aa0604801c1da9b222698edf7136efa18c8528
Sztyup <sztyup@gmail.com>
2017-12-26 05:25:42 +0100

    parse is seperated in permission and role

--- src/Acl/Permission.php
+++ src/Acl/Permission.php
@@ -6,15 +6,33 @@ class Permission extends Node
 {
     protected $title;
 
-    public function __construct($name, $title = '', $truth = null)
+    public function __construct($name, $title = '', $id = 0, $truth = null)
     {
         $this->title = $title;
 
-        parent::__construct($name, $truth);
+        parent::__construct($name, $truth, $id);
     }
 
     public function getTitle(): string
     {
         return $this->title ?? '';
+    }
+
+    protected static function parse($key, $node)
+    {
+        foreach ($node as $field) {
+            if (is_array($field)) {
+                $children = $field;
+            } elseif (is_string($field)) {
+                $title = $field;
+            } elseif (is_callable($field)) {
+                $truth = $field;
+            }
+        }
+
+        return [
+            new Permission($key, $title ?? '', $truth ?? null),
+            $children ?? []
+        ];
     }
 }
---
+++ src/Acl/Role.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Sztyup\Acl;
+
+class Role extends Node
+{
+    protected $title;
+    protected $description;
+    protected $permissions;
+
+    public function __construct(string $name, array $permissions, $title = '', $description = '', $truth = null)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->permissions = $permissions;
+
+        parent::__construct($name, $truth);
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getPermissions(): array
+    {
+        return $this->permissions;
+    }
+
+    protected static function parse($key, $node)
+    {
+        foreach ($node as $field) {
+            if (is_array($field)) {
+                $permissions = $field;
+            } elseif (is_string($field)) {
+                $title = $field;
+            } elseif (is_callable($field)) {
+                $truth = $field;
+            }
+        }
+
+        return [
+            new Role($key, $permissions ?? [], $title ?? '', $truth ?? null),
+            []
+        ];
+    }
+}
