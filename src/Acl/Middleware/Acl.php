3b23568f8e0173951c7eb4a9ae7e4c9d05cea036
Sztyup <sztyup@gmail.com>
2017-12-26 04:41:12 +0100

    ACL middleware handles authentication too

--- src/Acl/Middleware/Acl.php
+++ src/Acl/Middleware/Acl.php
@@ -6,20 +6,28 @@ use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Sztyup\Acl\Contracts\UsesAcl;
+use Sztyup\Acl\AclManager;
+use Sztyup\Acl\Contracts\HasAcl;
 use Sztyup\Acl\Exception\NotAuthorizedException;
 
 class Acl
 {
+    protected $acl;
+
+    public function __construct(AclManager $acl)
+    {
+        $this->acl = $acl;
+    }
+
     public function handle(Request $request, Closure $next)
     {
-        $action = $request->route()->getAction();
+        if ($auth = $this->parseAuth($request)) {
+            return $auth;
+        };
 
-        $user = $request->user();
-        $this->checkUser($user);
+        list($roles, $permissions) = $this->parseAcl($request);
 
-        $roles = Arr::wrap($action['is'] ?? []);
-        $permissions = Arr::wrap($action['can'] ?? []);
+        $user = $this->getUser($request);
 
         $missingRoles = [];
         $missingPermissions = [];
@@ -43,16 +51,55 @@ class Acl
         return $next($request);
     }
 
-    private function checkUser($user)
+    private function parseAuth(Request $request)
     {
+        $auth = $request->route()->getAction('auth');
+
+        if ($auth == null) { // Auth is not required
+            return false;
+        }
+
+        if ($user = $this->getUser($request)) { // If authenticated we are done
+            return false;
+        }
+
+        if ($request->ajax() || $request->wantsJson()) { // Dont redirect json
+            return response('Unauthorized.', 401);
+        }
+
+        if ($auth['target']) {
+            return redirect()->route('main.auth.redirect', $auth['target']);
+        }
+
+        if ($auth['route']) {
+            return redirect()->route($auth['route']);
+        }
+
+        throw new AuthenticationException();
+    }
+
+    private function parseAcl(Request $request)
+    {
+        $action = $request->route()->getAction();
+        return [
+            Arr::wrap($action['is'] ?? []),
+            Arr::wrap($action['can'] ?? [])
+        ];
+    }
+
+    private function getUser(Request $request): HasAcl
+    {
+        $user = $request->user();
         if ($user == null) {
             throw new AuthenticationException();
         }
 
         $reflection = new \ReflectionClass($user);
 
-        if (!$reflection->implementsInterface(UsesAcl::class)) {
+        if (!$reflection->implementsInterface(HasAcl::class)) {
             throw new \Exception('User doesnt implement Sztyup\Acl\Contracts\UsesAcl interface');
         }
+
+        return $user;
     }
 }
